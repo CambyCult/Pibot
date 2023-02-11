ac164026e358f2ed47a4283931989e613cb73bd1
Rickie Natividad <rickienatividad85@gmail.com>
2023-02-11 11:35:17 -0500

    adds jwt gem, user authentication, ability to create a new session by logging in

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   get "/users" => "users#index"
   get "/users/:id" => "users#show"
   post "/users" => "users#create"
+
+  #Login Session
+  post "/sessions" => "sessions#create"
 end
