11acbda9fd06f416a84c1cb33747f0afcde3d306
Rickie Natividad <rickienatividad85@gmail.com>
2023-02-11 11:42:31 -0500

    adds User update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,20 @@ class UsersController < ApplicationController
       render json: { errors: user.errors.full_messages }
     end
   end
+
+  def update
+    user = User.find_by(id: params["id"])
+
+    user.first_name = params["first_name"] || user.first_name
+    user.last_name = params["last_name"] || user.last_name
+    user.email = params["email"] || user.email
+    user.password = params["password"] || user.password
+    user.password_confirmation = params["password_confirmation"] || user.password_confirmation
+
+    if user.save
+      render json: { message: "user info updated" }
+    else
+      render json: { error: user.errors.full_messages }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  #User Actions
   get "/users" => "users#index"
   get "/users/:id" => "users#show"
   post "/users" => "users#create"
+  patch "/users/:id" => "users#update"
+  delete "/users/:id" => "users#destroy"
 
   #Login Session
   post "/sessions" => "sessions#create"
