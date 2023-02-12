c390ac20a44811f685db90463c9a9893e800d683
Rickie Natividad <rickienatividad85@gmail.com>
2023-02-11 23:37:12 -0500

    adds faker gem. adds loop to seeds.rb to generate users

--- Gemfile
+++ Gemfile
@@ -47,4 +47,5 @@ group :development do
 end
 
 gem 'jwt'
+gem 'faker'
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,30 @@
-User.create(
-  first_name: "jim",
-  last_name: "neville",
-  email: "jim@test.com",
-  password: "password",
-  password_confirmation: "password",
-  admin: true,
-)
+require "faker"
 
-User.create(
-  first_name: "gwen",
-  last_name: "rogers",
-  email: "gwen@test.com",
-  password: "password",
-  password_confirmation: "password",
-)
+# User.create(
+#   first_name: "jim",
+#   last_name: "neville",
+#   email: "jim@test.com",
+#   password: "password",
+#   password_confirmation: "password",
+#   admin: true,
+# )
+
+# User.create(
+#   first_name: "gwen",
+#   last_name: "rogers",
+#   email: "gwen@test.com",
+#   password: "password",
+#   password_confirmation: "password",
+# )
+
+11.times do
+  first_name = Faker::Name.first_name
+  users = User.new(
+    first_name: first_name,
+    last_name: Faker::Name.last_name,
+    email: first_name + "@test.com",
+    password: "password",
+    password_confirmation: "password",
+  )
+  users.save
+end
