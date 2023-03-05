21502eb262dbc8756df4fe170124cd54030e6335
Rickie Natividad <rickienatividad85@gmail.com>
2023-03-05 18:22:48 -0500

    Sessions controller generates user admin boolean key value pair for frontend authorization abilities

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
         Rails.application.credentials.fetch(:secret_key_base), # the secret key
         "HS256" # the encryption algorithm
       )
-      render json: { jwt: jwt, email: user.email, user_id: user.id }, status: :created
+      render json: { jwt: jwt, email: user.email, user_id: user.id, user_admin: user.admin }, status: :created
     else
       render json: {}, status: :unauthorized
     end
