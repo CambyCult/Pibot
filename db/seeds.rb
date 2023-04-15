ff859e0b31c935802a156ccf046200a57cd04429
Rickie Natividad <rickienatividad85@gmail.com>
2023-04-15 10:32:04 -0400

    adds seed data for BooleanChecklist

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,34 @@
-require "faker"
+# require "faker"
 
-User.create(
-  first_name: "jim",
-  last_name: "neville",
-  email: "jim@test.com",
-  password: "password",
-  password_confirmation: "password",
-  admin: true,
-)
+# User.create(
+#   first_name: "jim",
+#   last_name: "neville",
+#   email: "jim@test.com",
+#   password: "password",
+#   password_confirmation: "password",
+#   admin: true,
+# )
 
-12.times do
-  first_name = Faker::Name.unique.first_name
-  users = User.new(
-    first_name: first_name.downcase,
-    last_name: Faker::Name.last_name.downcase,
-    email: first_name.downcase + "@test.com",
-    password: "password",
-    password_confirmation: "password",
-  )
-  users.save
-end
+# 12.times do
+#   first_name = Faker::Name.unique.first_name
+#   users = User.new(
+#     first_name: first_name.downcase,
+#     last_name: Faker::Name.last_name.downcase,
+#     email: first_name.downcase + "@test.com",
+#     password: "password",
+#     password_confirmation: "password",
+#   )
+#   users.save
+# end
 
-6.times do
-  rig_name = Faker::GreekPhilosophers.unique.name
-  rigs = Rig.create!(
-    name: rig_name,
-  )
-end
+# 6.times do
+#   rig_name = Faker::GreekPhilosophers.unique.name
+#   rigs = Rig.create!(
+#     name: rig_name,
+#   )
+# end
 
-index = 1
+# index = 1
 # while index <= 6
 #   checklist = Checklist.new(
 #     rig_id: index,
@@ -46,9 +46,9 @@ while index <= 6
   checklist.save
 end
 
-Message.create!(
-  date: Time.now,
-  shift: "Second",
-  content: "Clean and well stocked. Weak AC.",
-  user_id: 6,
-)
+# Message.create!(
+#   date: Time.now,
+#   shift: "Second",
+#   content: "Clean and well stocked. Weak AC.",
+#   user_id: 6,
+# )
